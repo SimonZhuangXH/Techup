3acfeccc917b2e03fbc5eec521f1ca4f386b00e5
SimonZhuangXH <84184146+SimonZhuangXH@users.noreply.github.com>
2023-11-07 12:14:01 +0800

    init

---
+++ plan.py
@@ -0,0 +1,22 @@
+# EJS
+
+# cookies for multiple sessions
+
+# DB normalisation
+# ID (hashed by email and date) -> name
+# Email_ID -> textual content, summary
+# 
+
+# extracting content using chatbot??? host on
+
+# google auth Ruby on wheels???? Device gem?
+
+# OpenAI auth
+
+# Google analytics chunk
+
+# Email integration
+
+# zapier or workflow tool
+
+# distribution
