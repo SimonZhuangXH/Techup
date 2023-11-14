ba5d175825dfd84876e895b03595cc4644e5141d
SimonZhuangXH <84184146+SimonZhuangXH@users.noreply.github.com>
2023-11-14 19:39:28 +0800

    debug

--- server.js
+++ server.js
@@ -9,8 +9,9 @@ app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
     res.sendFile(`${publicPath}/boot.html`);
+    console.log("Sent")
   });
-
+console.log("proceed to listen")
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
