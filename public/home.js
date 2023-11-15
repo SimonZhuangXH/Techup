0368a2920bf0b5a126fe65b3b8283f76b1e51769
SimonZhuangXH <84184146+SimonZhuangXH@users.noreply.github.com>
2023-11-16 00:31:27 +0800

    align two pages

--- public/home.js
+++ public/home.js
@@ -1,36 +1,32 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
 anime.timeline({loop: false})
   .add({
-    targets: '.ml5 .line',
+    targets: '.ml11 .line',
+    scaleY: [0,1],
     opacity: [0.5,1],
-    scaleX: [0, 1],
-    easing: "easeInOutExpo",
+    easing: "easeOutExpo",
     duration: 700
-  }).add({
-    targets: '.ml5 .line',
-    duration: 600,
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
     easing: "easeOutExpo",
-    translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
+    duration: 700,
+    delay: 100
   }).add({
-    targets: '.ml5 .ampersand',
+    targets: '.ml11 .letter',
     opacity: [0,1],
-    scaleY: [0.5, 1],
     easing: "easeOutExpo",
     duration: 600,
-    offset: '-=600'
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
   }).add({
-    targets: '.ml5 .letters-left',
-    opacity: [0,1],
-    translateX: ["0.5em", 0],
+    targets: '.ml11 .line',
+    opacity: 0,
+    duration: 1000,
     easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=300'
-  }).add({
-    targets: '.ml5 .letters-right',
-    opacity: [0,1],
-    translateX: ["-0.5em", 0],
-    easing: "easeOutExpo",
-    duration: 600,
-    offset: '-=600'
+    delay: 500
   });
-
-  
